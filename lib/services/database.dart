16d864af6297599b2023a4f6b3d1cedd076f0b1a
Bossman Obboye <abless024@gmail.com>
2024-04-30 15:24:09 +0000

    push all

---
+++ lib/services/database.dart
@@ -0,0 +1,10 @@
+import  'package:cloud_firestore/cloud_firestore.dart'; 
+
+class FirebaseCRUD {
+  Future addEmployee(Map<String, dynamic> employeeDetails, String id) async {
+    return await FirebaseFirestore.instance
+        .collection('employees')
+        .doc(id)
+        .set(employeeDetails);
+  }
+}
